c9e5ba37d76926f67a2ee38a815f23b0a839fe5f
UseR <147231882+BogdanPryadko4853@users.noreply.github.com>
2024-06-19 15:30:15 +0300

    initialize

---
+++ src/main/java/org/rest/security/SpringSecurityJwtMasterApplication.java
@@ -0,0 +1,13 @@
+package org.rest.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityJwtMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rest/security/SpringSecurityJwtMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rest.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
