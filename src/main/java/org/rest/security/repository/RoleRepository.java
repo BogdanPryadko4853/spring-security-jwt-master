d3fdebe16c9610cbca4b2c165127a9a4c87b9644
UseR <147231882+BogdanPryadko4853@users.noreply.github.com>
2024-06-20 17:07:43 +0300

    add repository JPA (Role , User)

---
+++ src/main/java/org/rest/security/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package org.rest.security.repository;
+
+
+import org.rest.security.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
