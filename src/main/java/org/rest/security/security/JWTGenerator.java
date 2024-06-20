2598f7d63a6bbaa92af2bbd838eac15a5c0e16da
UseR <147231882+BogdanPryadko4853@users.noreply.github.com>
2024-06-21 00:41:34 +0300

    add jwt generator && entry point

---
+++ src/main/java/org/rest/security/security/JWTGenerator.java
@@ -0,0 +1,55 @@
+package org.rest.security.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JWTGenerator {
+
+    private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+
+    public String generateToken(Authentication authentication) {
+        String username = authentication.getName();
+        Date currentDate = new Date();
+        Date expireDate = new Date(currentDate.getTime() + SecurityConstants.JWT_EXPIRATION);
+
+        String token = Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt( new Date())
+                .setExpiration(expireDate)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+        System.out.println("New token :");
+        System.out.println(token);
+        return token;
+    }
+    public String getUsernameFromJWT(String token){
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return ((Claims) claims).getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception ex) {
+            throw new AuthenticationCredentialsNotFoundException("JWT was exprired or incorrect",ex.fillInStackTrace());
+        }
+    }
+
+}
---
+++ src/main/java/org/rest/security/security/SecurityConstants.java
@@ -0,0 +1,5 @@
+package org.rest.security.security;
+
+public class SecurityConstants {
+    public static final long JWT_EXPIRATION = 70000;
+}
